b42ceb2d4d8156d1da220d84b1626f16fc2df29a
Victor Fernández <victor.fernandez1006@gmail.com>
2024-12-30 00:04:40 -0500

    feature - add scope_read

--- src/main/java/com/coudevi/config/ResourceServerConfig.java
+++ src/main/java/com/coudevi/config/ResourceServerConfig.java
@@ -1,27 +1,32 @@
 package com.coudevi.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ResourceServerConfig {
     @Bean
-    public SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").hasAuthority("SCOPE_read")
+                        .requestMatchers("/api/**").hasAuthority("SCOPE_read") // Protege el recurso
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.decoder(jwtDecoder()))
                 );
         return http.build();
     }
+
     @Bean
     public JwtDecoder jwtDecoder() {
+        // Configuración explícita para manejar la validación de tokens de un único issuer
         return NimbusJwtDecoder.withJwkSetUri("http://localhost:9000/oauth2/jwks").build();
     }
 }
